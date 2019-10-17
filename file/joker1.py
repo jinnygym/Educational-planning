14d110e31e7f05cad4370a9b6ed77113b429d7f4
jinnygym <61070230@kmitl.ac.th>
2019-10-17 23:32:54 +0700

    Update

---
+++ file/joker1.py
@@ -0,0 +1,6 @@
+"""
+Project PSIT62
+Educational-planning
+>>Page: 1
+by Suphitsara Cheevanantaporn 61070230
+"""
--- file/plan1.py
+++ file/plan1.py
@@ -4,16 +4,29 @@ Educational-planning
 >>Page: 1
 by Suphitsara Cheevanantaporn 61070230
 """
-def login():
-    """login my App"""
-    print("\t\t$$$$$$$$$$   Welcome to Planer-Program  $$$$$$$$$$\n\n");
+import tkinter as tk
+plan = tk.Tk()
+plan.option_add("Font", "consolas 25")
+Label(plan, text="Educational", bg="orange").pack(fill=x)
+Label(plan, text="plan", bg="pink", width=15).pack()
+Label(plan, text="PSIT", fg="red", bg="bluesky")
+plan.title("I Love PSIT")
+for i in range(10):
+    tk.Label(plan, text="Welcome to Educational-planning Program ").grid() #Label >> widget
+plan.mainloop()
 
-        # print("\t   ====================================================\n")
-        # print("\t\t ___________   __            __________   ______   __\n")
-        # print("\t\t|   ______  | |  |          |   ____   | |      | |  |\n")
-        # print("\t\t|  |______| | |  |          |  |    |  | |   _  | |  |\n")
-        # print("\t\t|  _________| |  |          |  |____|  | |  | | |_|  |\n")
-        # print("\t\t|  |          |  |________  |  |    |  | |  | |      |\n")
-        # print("\t\t|__|          |___________| |__|    |__| |__| |______|\n\n")
-        # print("\t    ====================================================\n")
-login()
+
+
+# def login():
+#     """login my App"""
+#     print("\t\t$$$$$$$$$$   Welcome to Educational-planning Program  $$$$$$$$$$\n\n");
+
+#         # print("\t   ====================================================\n")
+#         # print("\t\t ___________   __            __________   ______   __\n")
+#         # print("\t\t|   ______  | |  |          |   ____   | |      | |  |\n")
+#         # print("\t\t|  |______| | |  |          |  |    |  | |   _  | |  |\n")
+#         # print("\t\t|  _________| |  |          |  |____|  | |  | | |_|  |\n")
+#         # print("\t\t|  |          |  |________  |  |    |  | |  | |      |\n")
+#         # print("\t\t|__|          |___________| |__|    |__| |__| |______|\n\n")
+#         # print("\t    ====================================================\n")
+# login()
