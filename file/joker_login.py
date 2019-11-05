af19a5cd2bfdbb325962090ebea99004b173f411
jinnygym <61070230@kmitl.ac.th>
2019-11-05 21:26:01 +0700

    test Educational-planning

--- file/joker_login.py
+++ file/joker_login.py
@@ -11,8 +11,9 @@ window = Tk()
 window.title("My Educational-planning")
 window.configure(background="black")
 
-Label (window, bg="orange") .grid(row=0, column=0, sticky=W)
+photol = PhotoImage(file="graduation.png")
+Label (window, image=photol, bg="black") .grid(row=0, column=0, sticky=W)
 #create label
-Label (window, text="Enter the word you would like a definition for:", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0, sticky=W)
+Label (window, text="$$$$$$$$$$  Welcome to Educational-planning  $$$$$$$$$$", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0, sticky=W)
 
 window.mainloop()
